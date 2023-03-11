7c9670a48fc427e873e4eac5c169c5d96a8b8086
Adam <iadamo.inc@gmail.com>
2023-03-11 14:35:06 -0800

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - main entry
+  * @argc: First parameter
+  * @argv: Second parameter
+  *
+  * Return: exit(EXIT_SUCCESS)
+  */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	exit(EXIT_SUCCESS);
+}

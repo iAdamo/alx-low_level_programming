c5bd72452f71b87588af40280a3b5f41bb5818b5
Adam <iadamo.inc@gmail.com>
2023-03-15 15:26:01 -0700

    0-create_array.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,20 +15,25 @@ int main(int argc, char *argv[])
 	int i;
 	int sum = 0;
 
-	for (i = 1; i < argc; i++)
+	if (argc == 1)
 	{
-		if (*argv[i] > '9' || *argv[i] < '0')
-		{
-			printf("Error\n");
-
-			return (1);
-		}
-		else
-		{
-			sum += atoi(argv[i]);
-		}
+		printf("0\n");
 	}
-	printf("%d\n", sum);
-
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!atoi(argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum += *argv[i];
+			}
+		}
+		printf("%d\n", sum);	
+	}
 	return (0);
 }

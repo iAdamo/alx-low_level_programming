cd424d609e1b58fd9f6e2f86339e5afa8fabfcd9
Adam <iadamo.inc@gmail.com>
2023-03-23 05:33:59 -0700

    typedef struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
   *
   * Description: Longer description
   */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 

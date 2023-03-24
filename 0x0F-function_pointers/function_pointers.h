38825d8dfbc27a4a253df40aa71c25af87c34acc
Adam <iadamo.inc@gmail.com>
2023-03-24 05:14:40 -0700

    Added header file

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int _putchar(char c);
+
+#endif

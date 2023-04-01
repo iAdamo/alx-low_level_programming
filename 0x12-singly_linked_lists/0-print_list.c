3a3a99e8aab82655c3dcc0bdc6e74901423c6acf
Adam <iadamo.inc@gmail.com>
2023-04-01 07:10:04 -0700

    Fixed IF condition

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,6 +18,14 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 		count++;
 		h = h->next;
 	}

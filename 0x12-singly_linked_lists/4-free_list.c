a5c746819367ae9ddaddab3235bcd001867f32a9
Adam <iadamo.inc@gmail.com>
2023-04-02 09:36:11 -0700

    Updtaed free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 /**
   * free_list -  function that frees a list_t list.
@@ -8,7 +7,7 @@
   */
 void free_list(list_t *head)
 {
-	list_t = *temp;
+	list_t *temp;
 
 	while (head != NULL)
 	{

0361007bb7b963a9f2a65b976d2ba70cc39522e4
Adam <iadamo.inc@gmail.com>
2023-04-02 08:40:16 -0700

    Added betty sytle to "free_list"

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-
+/**
+  * free_list -  function that frees a list_t list.
+  * @head: head list
+  */
 void free_list(list_t *head)
 {
 	free(head);

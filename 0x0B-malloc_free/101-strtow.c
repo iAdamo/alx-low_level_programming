e040238a8a30fcfa9c41e3d778c0f93551848546
Adam <iadamo.inc@gmail.com>
2023-06-15 15:21:22 +0100

    strings to words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,74 @@
+#include "main.h"
+/**
+ * ptrData - initializes data from a 1-di_array to 2-di_array
+ * @str: one dimensional pointer data to copy
+ * @ptr: two dimensional pointer
+ * Return: return two dimensional array of data
+*/
+void **ptrData(char *str, char **ptr)
+{
+	int i, j, b;
+	int a = 0;
+
+	for (i = 0; str[i]; i++)
+	{
+		b = 0;
+		for (j = i; str[j] != ' '; j++)
+		{
+			ptr[a][b] = str[j];
+			b++;
+		}
+		i = j;
+		if (b > 0)
+		{
+			a++;
+		}
+	}
+}
+/**
+ * strtow - splits a string into words
+ * @str: string
+ * Return: returns a pointer to an array of strings (words)
+*/
+char **strtow(char *str)
+{
+	char **ptr;
+	int i, j, b, len, row;
+	int col = 0, wid = 0, a = 0;
+
+	if (str == NULL || str == "")
+	{
+		return (NULL);
+	}
+	ptr = malloc(sizeof(char *) * col);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i]; i++)
+	{
+		row = 0;
+		while (str[i] != ' ')
+		{
+			row++;
+			i++;
+		}
+		if (row > 0)
+		{
+			ptr[wid] = malloc(sizeof(char) * row);
+			if (ptr[wid] == NULL)
+			{
+				for (a = 0; a <= wid; a++)
+				{
+					free(ptr[a]);
+				}
+				free(ptr);
+				return (NULL);
+			}
+			wid++;
+		}
+	}
+	ptr[wid] = NULL;
+	ptrData(str, ptr);
+	return (ptr);
+}

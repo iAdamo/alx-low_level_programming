fc54df295a5f5324aaf696f334e45162c15e0538
Adam <iadamo.inc@gmail.com>
2023-06-16 07:32:53 +0100

    Make changes but still needs major fixing

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,31 @@
 #include "main.h"
 /**
+ * wordCount - counts the amount of non spaced words in a string
+ * @str: strings to go through
+ * Return: number of non spaced words
+*/
+int wordCount(char *str)
+{
+	int i = 0, count = 0;
+	int flag;
+
+	while (str[i])
+	{
+		flag = 0;
+		while (str[i] != ' ')
+		{
+			flag = 1;
+			i++;
+		}
+		if (flag == 1)
+		{
+			count++;
+		}
+		i++;
+	}
+	return (count);
+}
+/**
  * ptrData - initializes data from a 1-di_array to 2-di_array
  * @str: one dimensional pointer data to copy
  * @ptr: two dimensional pointer
@@ -7,22 +33,25 @@
 */
 char **ptrData(char *str, char **ptr)
 {
-	int i, j, b;
-	int a = 0;
+	int i;
+	int a = 0, b = 0;
 
-	for (i = 0; str[i]; i++)
+	i = 0;
+	while (str[i])
 	{
 		b = 0;
-		for (j = i; str[j] != ' '; j++)
+		while (str[i] != ' ' && str[i] != '\0')
 		{
-			ptr[a][b] = str[j];
+			ptr[a][b] = str[i];
+			i++;
 			b++;
 		}
-		i = j;
 		if (b > 0)
 		{
+			ptr[a][b] = ' ';
 			a++;
 		}
+		i++;
 	}
 	return (ptr);
 }
@@ -33,43 +62,38 @@ char **ptrData(char *str, char **ptr)
 */
 char **strtow(char *str)
 {
+	int i = 0;
+	int word, len;
+	int width = 0;
 	char **ptr, **ptrs;
-	int i, row;
-	int col = 0, wid = 0, a = 0;
 
 	if (str == NULL || *str == '\0')
 	{
 		return (NULL);
 	}
-	ptr = malloc(sizeof(char *) * col);
+	word = wordCount(str);
+	ptr = malloc(sizeof(char *) * word);
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; str[i]; i++)
+	i = 0;
+	while (str[i])
 	{
-		row = 0;
-		while (str[i] != ' ')
+		len = 0;
+		while (str[i] != ' ' && str[i] != '\0')
 		{
-			row++;
+			len++;
 			i++;
 		}
-		if (row > 0)
+		if (len > 0)
 		{
-			ptr[wid] = malloc(sizeof(char) * row);
-			if (ptr[wid] == NULL)
-			{
-				for (a = 0; a <= wid; a++)
-				{
-					free(ptr[a]);
-				}
-				free(ptr);
-				return (NULL);
-			}
-			wid++;
+			ptr[width] = malloc(sizeof(char) * (len + 1));
+			width++;
 		}
+		i++;
 	}
-	ptr[wid] = NULL;
+	ptr[width] = NULL;
 	ptrs = ptrData(str, ptr);
 	return (ptrs);
 }

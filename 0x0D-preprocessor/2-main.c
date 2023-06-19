ad2ae5364cf9dfaaf50cebea1720f7d74db097ef
Adam <iadamo.inc@gmail.com>
2023-06-19 13:17:24 +0100

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+/**
+ * main - program that prints the name of the file it was compiled from
+ *
+ * Return: return 0 success
+*/
+int main(void)
+{
+	puts(__FILE__);
+
+	return (0);
+}

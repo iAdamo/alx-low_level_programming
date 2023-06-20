d303c743eb3ce22bae0dcad824e36bb02f8dc520
Adam <iadamo.inc@gmail.com>
2023-06-20 11:26:35 +0100

    updated function prototype to header file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,7 +8,7 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = "Poppy";
-	d->age = 3.5;
-	d->owner = "Bob";
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }

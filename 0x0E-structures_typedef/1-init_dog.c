12bb9370d7a590794db16160fd568b4af6b52845
Adam <iadamo.inc@gmail.com>
2023-06-20 11:21:15 +0100

    function var init

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - function that initialize a variable of type struct dog
+ * @d: pointer to struct data from outside the function
+ * @name: first pointer variable to string
+ * @age: second variable to a float number
+ * @owner: third pointer variable to string
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = "Poppy";
+	d->age = 3.5;
+	d->owner = "Bob";
+}

01293f3b7a02310564d9613d857df8ec6622aa0a
Adam <iadamo.inc@gmail.com>
2023-06-20 12:23:06 +0100

    a new name for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,10 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - typedef to struct dog
+*/
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);

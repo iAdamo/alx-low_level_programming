1e2372911da98b7af9aeec160736937279865a6f
Adam <iadamo.inc@gmail.com>
2023-06-20 06:26:44 +0100

    fixes struct data

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
-
-/*struct dog - struct to hold dog infos*/
-struct dog {
-	name char*;
-	age float;
-	owner char *;
+/**
+ * struct dog - struct to hold dog infos
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the dog's owner
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif

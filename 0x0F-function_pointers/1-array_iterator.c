e224a186f4caa5b178d576dcb035a9b49c5891b5
Adam <iadamo.inc@gmail.com>
2023-06-23 12:22:50 +0100

    fix bugs

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,11 +7,12 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	size_t i;
+
 	if (array == NULL || action == NULL)
 	{
 		return;
 	}
-	size_t i;
 
 	for (i = 0; i < size; i++)
 	{

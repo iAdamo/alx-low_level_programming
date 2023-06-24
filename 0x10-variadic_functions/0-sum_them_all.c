41a9bb135604db2a014fe8b070fd1037fafad656
Adam <iadamo.inc@gmail.com>
2023-06-24 17:55:28 +0100

    satisfying betty code style

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,6 +2,7 @@
 /**
  * sum_them_all - sum up all parameters
  * @n: count of parameters
+ * @...: ellipsis containing all other passed arguments
  * Return: returns the sum of all its parameters
 */
 int sum_them_all(const unsigned int n, ...)

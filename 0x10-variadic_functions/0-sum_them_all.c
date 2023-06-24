c554c4127b12b2ddcbed3c24483d3585e66de291
Adam <iadamo.inc@gmail.com>
2023-06-24 17:57:15 +0100

    satisfying betty code style

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,14 +1,18 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+
 /**
- * sum_them_all - sum up all parameters
- * @n: count of parameters
- * @...: ellipsis containing all other passed arguments
- * Return: returns the sum of all its parameters
-*/
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
+ *
+ * Return: If n == 0 - 0.
+ *         Otherwise - the sum of all parameters.
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i, sum = 0;
 	va_list arg;
+	unsigned int i, sum = 0;
 
 	va_start(arg, n);
 

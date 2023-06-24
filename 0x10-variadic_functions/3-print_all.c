f0000534240e21db8adcd1cd593c16c7c50cddc1
Adam <iadamo.inc@gmail.com>
2023-06-24 20:15:12 +0100

    function that prints all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+/**
+ * print_all - function that prints anything
+ * @format: format identifiers
+*/
+void print_all(const char * const format, ...)
+{
+	int i;
+	char *str;
+	va_list arg;
+
+	if (format == NULL)
+	{
+		return;
+	}
+	va_start(arg, format);
+	i = 0;
+	while (format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(arg, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(arg, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(arg, double));
+			break;
+		case 's':
+			str = va_arg(arg, char*);
+			if (str != NULL)
+				printf("%s", str);
+			else
+				printf("(nil)");
+			break;
+		default:
+			i++;
+			continue;
+		}
+		if (format[i + 1] != '\0')
+			printf(", ");
+		i++;
+	}
+	putchar('\n');
+}

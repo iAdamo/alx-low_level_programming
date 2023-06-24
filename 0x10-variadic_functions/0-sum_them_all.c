fb8e14e43ea029909ea435d65e39f817695c14ff
Adam <iadamo.inc@gmail.com>
2023-06-24 09:38:57 +0100

    fixing betty style

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,8 +6,7 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
-	int sum = 0;
+	unsigned int i, sum = 0;
 	va_list arg;
 
 	if (n == 0)
@@ -19,7 +18,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(arg, unsigned int);
+		sum += va_arg(arg, int);
 	}
 	va_end(arg);
 	return (sum);

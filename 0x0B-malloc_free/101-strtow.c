1131474cf6d313cc87cccedc3c01f8c77508884f
Adam <iadamo.inc@gmail.com>
2023-07-02 00:17:50 +0100

    made some changes

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,23 +1,47 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * initializes data from a 1-di_array to 2-di_array
+ * @str: one dimensional pointer data to copy
+ * @ptr: two dimensional pointer
+ * Return: return two dimensional array of data
+*/
+char **finalptr(char *str, char **ptr)
+{
+	int i = 0, j = 0, k = 0;
+
+	while (str[i])
+	{
+		if (str[i] != ' ')
+		{
+			ptr[j][k] = str[i];
+			k++;
+			if (str[i + 1] == ' ' || str[i + 1] == '\0')
+			{
+				ptr[j][k] = '\0';
+				j++;
+				k = 0;
+			}
+		}
+		i++;
+	}
+	return (ptr);
+}
 /**
  * wordCount - counts the amount of non spaced words in a string
  * @str: strings to go through
  * Return: number of non spaced words
 */
-int wordCount(char *str)
+int wordcount(char *str)
 {
-	int i = 0, count = 0;
-	int flag;
+	int i;
+	int count = 0;
 
+	i = 0;
 	while (str[i])
 	{
-		flag = 0;
-		while (str[i] != ' ')
-		{
-			flag = 1;
-			i++;
-		}
-		if (flag == 1)
+		if ((str[i] != ' ') && (str[i + 1] == ' ' || str[i + 1] == '\0'))
 		{
 			count++;
 		}
@@ -26,74 +50,47 @@ int wordCount(char *str)
 	return (count);
 }
 /**
- * ptrData - initializes data from a 1-di_array to 2-di_array
- * @str: one dimensional pointer data to copy
- * @ptr: two dimensional pointer
- * Return: return two dimensional array of data
-*/
-char **ptrData(char *str, char **ptr)
-{
-	int i;
-	int a = 0, b = 0;
-
-	i = 0;
-	while (str[i])
-	{
-		b = 0;
-		while (str[i] != ' ' && str[i] != '\0')
-		{
-			ptr[a][b] = str[i];
-			i++;
-			b++;
-		}
-		if (b > 0)
-		{
-			ptr[a][b] = ' ';
-			a++;
-		}
-		i++;
-	}
-	return (ptr);
-}
-/**
  * strtow - splits a string into words
  * @str: string
  * Return: returns a pointer to an array of strings (words)
 */
 char **strtow(char *str)
 {
-	int i = 0;
-	int word, len;
-	int width = 0;
-	char **ptr, **ptrs;
+	int i, len;
+	int a = 0, count = 0;
+	char **ptr, **final;
 
-	if (str == NULL || *str == '\0')
-	{
+	if (str == NULL)
 		return (NULL);
-	}
-	word = wordCount(str);
-	ptr = malloc(sizeof(char *) * word);
+	len = wordcount(str);
+	ptr = malloc(sizeof(char *) * (len + 1));
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
+
 	i = 0;
 	while (str[i])
 	{
-		len = 0;
-		while (str[i] != ' ' && str[i] != '\0')
+		if (str[i] != ' ')
 		{
-			len++;
-			i++;
-		}
-		if (len > 0)
-		{
-			ptr[width] = malloc(sizeof(char) * (len + 1));
-			width++;
+			count++;
+			if (str[i + 1] == ' ' || str[i + 1] == '\0')
+			{
+				ptr[a] = malloc(sizeof(char) * (count + 1));
+				if (ptr[a] == NULL)
+				{
+					for (a = a - 1; a >= 0; a--)
+					{
+						free(ptr[a]);
+					}
+					free(ptr);
+					return (NULL);
+				}
+				a++;
+				count = 0;
+			}
 		}
 		i++;
 	}
-	ptr[width] = NULL;
-	ptrs = ptrData(str, ptr);
-	return (ptrs);
+	final = finalptr(str, ptr);
+	return (final);
 }

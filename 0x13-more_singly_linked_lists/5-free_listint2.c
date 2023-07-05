dc9f505d12a81397fb661a169a72081ff89888b7
Adam <iadamo.inc@gmail.com>
2023-07-05 19:12:08 +0100

    removed unitended lines

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -21,22 +21,3 @@ void free_listint2(listint_t **head)
 		*head = temp;
 	}
 }
-
-int main(void)
-{
-    listint_t *head;
-
-    head = NULL;
-    add_nodeint_end(&head, 0);
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 2);
-    add_nodeint_end(&head, 3);
-    add_nodeint_end(&head, 4);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 402);
-    add_nodeint_end(&head, 1024);
-    print_listint(head);
-    free_listint2(&head);
-    printf("%p\n", (void *)head);
-    return (0);
-}

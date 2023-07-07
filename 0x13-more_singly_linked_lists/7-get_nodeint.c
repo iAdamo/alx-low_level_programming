1003501cb415046f174860153c5758f37a5c2b00
Adam <iadamo.inc@gmail.com>
2023-07-07 07:09:17 +0100

    fixing node distribution

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,6 +10,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 	listint_t *node;
 
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (0);
+
 	if (head == NULL)
 		return (0);
 
@@ -20,8 +24,29 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		temp = temp->next;
 		index--;
 	}
-	node = temp;
-
+	node->n = temp->n;
+	node->next = NULL;
 
 	return (node);
+}
+int main(void)
+{
+    listint_t *head;
+    listint_t *node;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+    node = get_nodeint_at_index(head, 5);
+    printf("%d\n", node->n);
+    print_listint(head);
+    free_listint2(&head);
+    return (0);
 }

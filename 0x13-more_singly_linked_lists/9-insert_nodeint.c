9f110258270c65421f8b8bc4d38db12bbb979261
Adam <iadamo.inc@gmail.com>
2023-07-07 22:41:21 +0100

    fixed function escessive index request

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,9 +21,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (*head == NULL)
 	{
 		*head = node;
-		return (*head);
 	}
-	if (idx == 0)
+	else if (idx == 0)
 	{
 		node->next = *head;
 		*head = node;
@@ -35,10 +34,32 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		while (idx > 1)
 		{
 			temp = temp->next;
+			if (temp == NULL)
+				return (NULL);
 			idx--;
 		}
 		node->next = temp->next;
 		temp->next = node;
 	}
 	return (*head);
+}
+int main(void)
+{
+    listint_t *head;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+    printf("-----------------\n");
+    insert_nodeint_at_index(&head, 15, 4096);
+    print_listint(head);
+    free_listint2(&head);
+    return (0);
 }

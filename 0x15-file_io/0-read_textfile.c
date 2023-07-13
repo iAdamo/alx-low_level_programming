43127704deb3cdb1fe20b7f4391a35e5cadcc127
Adam <iadamo.inc@gmail.com>
2023-07-13 01:50:55 +0100

    fixed indentations

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,23 +17,27 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
 		close(fd);
 		return (0);
-
+	}
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
+	{
 		close(fd);
 		return (0);
-
+	}
 	bytes = read(fd, buffer, letters);
 	if (bytes == -1)
+	{
 		close(fd);
 		return (0);
-
+	}
 	if (write(STDIN_FILENO, buffer, bytes) == -1)
+	{
 		close(fd);
 		return (0);
-
+	}
 	free(buffer);
 	close(fd);
 	return (bytes);

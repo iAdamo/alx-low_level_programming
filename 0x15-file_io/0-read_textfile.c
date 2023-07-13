3a27d5aa763ec8ef8dc48602d1fad81a64f2d95e
Adam <iadamo.inc@gmail.com>
2023-07-13 01:45:12 +0100

    fixed closing ans mem alloc

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
-	ssize_t byte;
+	ssize_t bytes;
 	int fd;
 
 	if (filename == NULL)
@@ -17,18 +17,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+		close(fd);
 		return (0);
 
-	buffer = malloc(4096);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
+		close(fd);
 		return (0);
-	byte = read(fd, buffer, letters);
-	if (byte == -1)
+
+	bytes = read(fd, buffer, letters);
+	if (bytes == -1)
+		close(fd);
 		return (0);
-	if (write(STDIN_FILENO, buffer, letters) == -1)
+
+	if (write(STDIN_FILENO, buffer, bytes) == -1)
+		close(fd);
 		return (0);
+
 	free(buffer);
 	close(fd);
-
-	return (byte);
+	return (bytes);
 }

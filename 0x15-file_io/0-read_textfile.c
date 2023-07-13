f93f0faa603cc4a014ea123570361c1364797b34
Adam <iadamo.inc@gmail.com>
2023-07-13 07:58:48 +0100

    got the issue(return variable)

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -28,6 +28,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	bytes = read(fd, buffer, letters);
 	if (bytes == -1)
 	{
+		close(fd);
 		free(buffer);
 		return (0);
 	}
@@ -36,5 +37,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	close(fd);
 	if (rtrn == -1)
 		return (0);
-	return (bytes);
+	return (rtrn);
 }

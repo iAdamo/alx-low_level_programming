ea2da39d49cbee062b187eec0da76258d7056445
Adam <iadamo.inc@gmail.com>
2023-07-13 23:33:02 +0100

    create files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,23 +7,22 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, len = 0;
 	ssize_t bytes;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	bytes = write(fd, text_content, strlen(text_content));
-	if (bytes == -1)
-	{
-		close(fd);
-		return (-1);
-	}
+	while (text_content && *(text_content + len))
+		len++;
 
+	bytes = write(fd, text_content, len);
 	close(fd);
+	if (bytes == -1)
+		return (-1);
 	return (1);
 }

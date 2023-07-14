d6b25122bd71368c224951d5f963475d9e7af8ed
Adam <iadamo.inc@gmail.com>
2023-07-14 17:38:44 +0100

    ELF header

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point for program to get header of ELF file
+ * @ac: The number of arguments
+ * @av: The pointer to array of arguments
+ * Return: 1 on success, error code on failure
+ */
+int main(int ac, char *av[])
+{
+	printf("argc:%d, argv:%p\n", ac, (void *)*av);
+	return (1);
+}

ca8ceb2cb79ef6eedcfc5916da32b76c4d0e98dc
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-08-20 14:57:14 +0100

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,38 +11,42 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *node, *temp;
 
-	if (*h == NULL && idx > 0)
-		return (NULL);
-
 	node = malloc(sizeof(dlistint_t));
 	if (node == NULL)
 		return (NULL);
 	node->n = n;
-	if  (idx == 0)
+
+	if (idx == 0)
 	{
 		if (*h == NULL)
-		{
 			*h = node;
-			return (*h);
-		}
-		node->next = *h;
-		(*h)->prev = node;
-		*h = node;
-	}
-	else
-	{
-		temp = *h;
-		while (idx > 1)
+		else
 		{
-			temp = temp->next;
-			if (temp == NULL)
-				return (NULL);
-			idx--;
+			node->next = *h;
+			(*h)->prev = node;
+			*h = node;
 		}
-		temp->next->prev = node;
-		node->prev = temp;
-		node->next = temp->next;
-		temp->next = node;
+		return (*h);
 	}
+	temp = *h;
+	while (idx > 1)
+	{
+		temp = temp->next;
+		idx--;
+	}
+	if (temp->next == NULL)
+	{
+		temp->next = node;
+		node->prev = temp;
+		return (*h);
+	}
+	if (temp == NULL)
+		return (*h);
+
+	temp->next->prev = node;
+	node->prev = temp;
+	node->next = temp->next;
+	temp->next = node;
+
 	return (*h);
 }

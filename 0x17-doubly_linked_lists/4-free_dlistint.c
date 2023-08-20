f6a22d7d69daf33210aaf5b3014607216bede146
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-08-20 03:11:11 +0100

    Free list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -6,11 +6,12 @@
 */
 void free_dlistint(dlistint_t *head)
 {
+	if (head == NULL)
+		return;
 	while (head->next)
 	{
 		head = head->next;
 		free(head->prev);
-		head->prev = NULL;
 	}
 	free(head);
 }

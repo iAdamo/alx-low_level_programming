27d08fa410fab8c34548643c70df5a066759fc10
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-09-08 13:49:12 -0700

    fixed test case 2048

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,11 +16,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = calloc(sizeof(hash_node_t *), table->size);
+	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
 		return (NULL);
 
-	for (i = 0; i < table->size; i++)
+	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
 	return (table);

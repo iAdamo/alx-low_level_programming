8d3afa04726c2af0d904f0f4c1db2e9a5d0266c0
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-09-09 20:51:59 +0100

    fixed betty max line  style

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,16 +1,13 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
-/*==========================Standard Libraris============================================*/
-
+/*==========================Standard Libraris=============================*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/*=======================================================================================*/
 
-/*========Struct data structure==========================================================*/
-
+/*========Struct data structure===========================================*/
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -40,9 +37,7 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-/*=======================================================================================*/
-
-/*===============================Function Prototypes=====================================*/
+/*===============================Function Prototypes======================*/
 hash_table_t *hash_table_create(unsigned long int size);
 
 #endif

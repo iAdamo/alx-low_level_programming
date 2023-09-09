107a17657551d3d74c02e4f18eed705e8caab7c2
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-09-09 20:44:08 +0100

    revert back to former

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,19 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
-	hash_table_t *ht;
+	size_t i;
+	hash_table_t *table;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
 		return (NULL);
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+		table->array[i] = NULL;
 
-	return (ht);
+	return (table);
 }

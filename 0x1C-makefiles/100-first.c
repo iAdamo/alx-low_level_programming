8b5281715a9ab53cf60e0a1ce2f76efaa5cf60f4
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-11-02 18:20:30 +0100

    makefiles

---
+++ 0x1C-makefiles/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * before_main - prints a string before main executes
+ *
+ * Return: nothing
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x1C-makefiles/README.md
+++ 0x1C-makefiles/README.md
@@ -1,2 +1,2 @@
-### C-Makefiles
-**`c`**
+### C - Makefiles
+**`C`**
